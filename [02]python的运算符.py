8b614bef2e1a4632c7aee59e9654506ec731e362
zhouyongcai <807923011@qq.com>
2023-10-26 16:36:46 +0800

    第8次修改

---
+++ [02]python的运算符.py
@@ -0,0 +1,37 @@
+# # 加
+# a = 1+1
+# # 减
+# b = 2-1
+# #变量之间也可以做运算
+# c = 4
+# d = 3
+# e = c-d
+# # 乘
+# f = 1*2
+# # 除
+# g = 3/2
+# # 取模（取余）
+# h = 5%2
+# # 幂
+# i = 2**3
+# # 取整除
+# j = 3//2
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+# print(i)
+# print(j)
+
+a = 1
+b = 2
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a <= b)
+print(a >= b)
