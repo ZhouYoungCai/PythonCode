893d2d30d7e52c718ddd2eb62c654deea733d461
zhouyongcai <807923011@qq.com>
2023-11-15 10:06:43 +0800

    第1修改

---
+++ DemoCode/python_list_test.py
@@ -0,0 +1,15 @@
+li1 = list()  # 空列表
+li2 = list('hogwarts')  # 字符串
+li3 = list((1, 2, 3))  # 元组
+li4 = list({4, 5, 6})  # 集合
+li5 = list({'a': 7, 'b': 8})  # 字典
+print(type(li1), li1)
+print(type(li2), li2)
+print(type(li3), li3)
+print(type(li4), li4)
+print(type(li5), li5)
+
+li6 = [1,2,3,4,5]
+print(li6[0])  #打印1
+print(li6[3])  #打印4
+print(li6[-1])  #打印5
