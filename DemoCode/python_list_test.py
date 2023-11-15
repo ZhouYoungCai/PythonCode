76fd48e59d081c03b48a7a550d434edcb3eb2de0
zhouyongcai <807923011@qq.com>
2023-11-15 10:57:15 +0800

    第1修改

--- DemoCode/python_list_test.py
+++ DemoCode/python_list_test.py
@@ -6,34 +6,33 @@ li2 = list('hogwarts')  # 字符串，创建列表
 li3 = list((1, 2, 3))  # 元组，创建列表
 li4 = list({4, 5, 6})  # 集合，创建列表
 li5 = list({'a': 7, 'b': 8})  # 字典，创建列表
-print(type(li1), li1)
-print(type(li2), li2)
-print(type(li3), li3)
-print(type(li4), li4)
-print(type(li5), li5)
+print(type(li1), li1,type(li2), li2,type(li3), li3,type(li4), li4,type(li5), li5)
+
 
 li6 = [1,2,3,4,5]
 print(li6[0])  # 打印1,索引的使用
 print(li6[3])  # 打印4,索引的使用
-print(li6[-1])  # 打印5,索引的使用
+print(f"list6最后一个元素是：",li6[-1])  # 打印5,索引的使用
 
 
 list7 = [1,2,3]
-print(1 in list7)      # 成员检测，in：检查一个元素是否在列表中
-print(2 not in list7)  # 成员检测，，not in：检查一个元素不在列表中
-
-list8 = [1,2,3]
-list8.append("hogwarts")  # append():讲一个对象添加到列表的末尾
-print(list8)
-
-list9 = [1,2,3]
-list10 = [4,5,6]
-list9.extend("list10")  # extend():将一个可迭代对象的所有元素，添加到列表末尾
-print(list9)
+print(f"判断1在list7里面：",1 in list7)      # 成员检测，in：检查一个元素是否在列表中
+print(f"判断2不在list7里面：",2 not in list7)  # 成员检测，，not in：检查一个元素不在列表中
 
 list11 = [3]
 print(list11 * 7)  # *乘号的使用
 
-list12 = [3,4,5]
-list13 = [1,2,6]
-print(list12 + list13)  # +加号的使用
+list12 = [1,2,3]
+list13 = [4,5,6]
+print(f"list12+list13的值是：",list12 + list13)  # +加号的使用
+
+list8 = [1,2,3]
+list8.append("hogwarts")  # append():append方法讲一个对象添加到列表的末尾
+print(f"list8.append后的结果是：",list8,f"list8.append后的长度是：",len(list8))
+
+list9 = [1,2,3]
+list10 = [4,5,6]
+list9.extend(list10)  # extend():extend方法将一个可迭代对象的所有元素，添加到列表末尾
+list10.extend("789")  # extend():extend方法将一个可迭代对象的所有元素，添加到列表末尾
+print(f"list9.extend后的结果是：",list9)
+print(f"list10.extend后的结果是：",list10)
