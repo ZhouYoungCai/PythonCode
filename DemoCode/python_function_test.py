b123cd0b05dd565d2df5c02b3d59bc76c6e65646
zhouyongcai <807923011@qq.com>
2023-11-21 17:18:06 +0800

    第1修改

--- DemoCode/python_function_test.py
+++ DemoCode/python_function_test.py
@@ -5,7 +5,7 @@
 # 定义函数
 def func_demo():
     # 函数体
-    print(f"这是一个函数")
+    print(f"这是一个函数",1)
 
 
 def func_with_params(a, b, c):
@@ -15,8 +15,8 @@ def func_with_params(a, b, c):
     print(f"传入的参数为：a={a},b={b},c={c}")
 
 # 打印函数 comments 的内容
-# print(func_with_params.__doc__)
-# help(func_with_params)
+print(f"打印函数 comments 的内容",func_with_params.__doc__)
+help(func_with_params)
 
 # 定义空函数
 def filter_char(s):
@@ -35,8 +35,8 @@ def person(name, age, nationality="中国"):
         print(f"{name} 未成年")
 
 # 指定参数默认值错误
-# def wrong_demo(a=1, b, c):
-#     pass
+#def wrong_demo(a=1, b, c):
+#    pass
 
 # 错误示范，默认值为空列表
 # 默认值一定要用不可变对象，否则的话默认值可能会随着调用发生变化
@@ -58,15 +58,15 @@ def sum(a, b):
 r = sum(1, 2)
 print(r)
 
-# 调用函数
-# func_demo()
-# 位置参数
-# func_with_params(1, 2, 3)
-# 位置参数错误例子，数量错误
-# func_with_params(1, 2, 3, 4)
-# func_with_params(2, 1, 3)
-# 位置参数错误例子，顺序错误
-# person(20, "lily")
-# person("lily", 20)
-# 关键字传参
-# person(age=12, name="Tom",nationality="美国")
+
+func_demo()  #调用函数
+
+func_with_params(1, 2, 3)  #位置参数
+
+#func_with_params(1, 2, 3, 4)   #位置参数错误例子，数量错误
+func_with_params(2, 1, 3)
+
+#person(20, "lily")   #位置参数错误例子，顺序错误
+person("lily", 20)
+
+person(age=12, name="Tom",nationality="美国")  #关键字传参
